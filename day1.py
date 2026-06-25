77fdbe75f05ec8541253e4a38dac48023f15bf88
Pradip Mainali <pradipmainai5@gmail.com>
2026-06-25 01:10:01 +0100

    Start Mission Korea daily log

---
+++ day1.py
@@ -0,0 +1,12 @@
+name = "Pradip"
+country =  "Nepal"
+current_place = "UK"
+goal = "Robotic Master in South Korea"
+
+print("My name is: ", name)
+print("I am from: ", country)
+print("I currently live at  : ", current_place)
+print("My goal is : ", goal)
+
+
+
---
+++ day2.py
@@ -0,0 +1,9 @@
+name = input("What is your  name?" )
+country  = input("Where are you from?" )
+goal =  input("What is your goal? " )
+
+print("---------- My Mission Profile -------------")
+print("Name:", name) 
+print("Country: " , country)
+print("Goal: " , goal)
+print("I am preparing for my mssion korea 2028")
