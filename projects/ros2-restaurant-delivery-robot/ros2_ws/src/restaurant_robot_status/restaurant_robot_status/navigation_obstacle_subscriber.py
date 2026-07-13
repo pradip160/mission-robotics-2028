0e3ba301b92a50e69e641b06f6aaa75809944a82
Pradip Mainali <pradipmainai5@gmail.com>
2026-07-13 23:53:58 +0100

    WIP: start ROS2 sensor timeout safety

--- projects/ros2-restaurant-delivery-robot/ros2_ws/src/restaurant_robot_status/restaurant_robot_status/navigation_obstacle_subscriber.py
+++ projects/ros2-restaurant-delivery-robot/ros2_ws/src/restaurant_robot_status/restaurant_robot_status/navigation_obstacle_subscriber.py
@@ -7,8 +7,15 @@ class NavigationObstacleSubscriber(Node):
 
     def __init__(self):
         super().__init__('navigation_obstacle_subscriber')
+        self.sensor_timeout = 3.0
         self.camera_state = 'UNKNOWN'
+        self.last_camera_message_time = None
+        self.camera_fresh = False
+
         self.lidar_state = 'UNKNOWN'
+        self.last_lidar_message_time = None 
+        self.lidar_fresh = False
+
         self.subscription = self.create_subscription(
             String,
             'obstacle_status',
@@ -30,7 +37,10 @@ class NavigationObstacleSubscriber(Node):
         )
   
 
-   
+        self.saftey_timer = self.create_timer(
+            1.0,
+            self.check_sensor_freshness
+        )    
 
 
     def receive_obstacle(self, message):
@@ -45,6 +55,10 @@ class NavigationObstacleSubscriber(Node):
 
     def decide_navigation(self):
         motor_command = String()
+
+        if not self.camera_fresh or not self.lidar_fresh:
+             motor_command.data = 'STOP'
+
         if self.camera_state == 'UNKNOWN' or self.lidar_state == 'UNKNOWN':
              motor_command.data = 'STOP'
 
@@ -62,6 +76,25 @@ class NavigationObstacleSubscriber(Node):
              f'Camera: {self.camera_state}, LiDAR: {self.lidar_state}, Command: {motor_command.data}'
         )
 
+    def check_sensor_freshness(self):
+        current_time = self.get_clock().now()
+
+        if self.last_camera_message_time is None or self.last_lidar_message_time is None:
+            self.camera_fresh = False
+            self.lidar_fresh = False
+            self.decide_navigation()
+            return
+        
+        camera_age = current_time - self.last_camera_message_time
+        lidar_age = current_time - self.last_lidar_message_time
+
+        camera_age_seconds = camera_age.nanoseconds / 1_000_000_000
+        lidar_age_seconds = lidar_age.nanoseconds / 1_000_000_000
+
+        self.camera_fresh = camera_age_seconds <= self.sensor_timeout
+        self.lidar_fresh = lidar_age_seconds <= self.sensor_timeout
+
+        self.decide_navigation()
 
 def main(args=None):
     rclpy.init(args=args)
