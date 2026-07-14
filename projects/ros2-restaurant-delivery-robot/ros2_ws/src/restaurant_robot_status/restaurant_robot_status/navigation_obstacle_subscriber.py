7279f6bd0902ec25ffd0743126e64fed4b3d11e9
Pradip Mainali <pradipmainai5@gmail.com>
2026-07-14 23:12:45 +0100

    Add directional navigation checkpoint

--- projects/ros2-restaurant-delivery-robot/ros2_ws/src/restaurant_robot_status/restaurant_robot_status/navigation_obstacle_subscriber.py
+++ projects/ros2-restaurant-delivery-robot/ros2_ws/src/restaurant_robot_status/restaurant_robot_status/navigation_obstacle_subscriber.py
@@ -8,6 +8,16 @@ class NavigationObstacleSubscriber(Node):
     def __init__(self):
         super().__init__('navigation_obstacle_subscriber')
         self.sensor_timeout = 3.0
+        self.front_state = 'UNKNOWN'
+
+        self.left_state = 'UNKNOWN'
+        self.last_left_message_time = None
+        self.left_fresh = False 
+ 
+        self.right_state = 'UNKNOWN'
+        self.last_right_message_time = None 
+        self.right_fresh = False
+
         self.camera_state = 'UNKNOWN'
         self.last_camera_message_time = None
         self.camera_fresh = False
@@ -35,6 +45,18 @@ class NavigationObstacleSubscriber(Node):
             self.receive_lidar,
             10
         )
+        self.left_subscription = self.create_subscription(
+            String,
+            'left_status',
+            self.receive_left,
+            10
+        )
+        self.right_subscription = self.create_subscription(
+            String,
+            'right_status',
+            self.receive_right,
+            10
+        )
   
 
         self.safety_timer = self.create_timer(
@@ -45,35 +67,51 @@ class NavigationObstacleSubscriber(Node):
 
     def receive_obstacle(self, message):
         self.camera_state = message.data
+        self.last_camera_message_time = self.get_clock().now()
         self.decide_navigation()
 
 
 
     def receive_lidar(self, message):
         self.lidar_state = message.data 
+        self.last_lidar_message_time = self.get_clock().now()
+        self.decide_navigation()
+
+    def receive_left(self, message):
+        self.left_state = message.data 
+        self.last_left_message_time = self.get_clock().now()
+        self.decide_navigation()
+
+    def receive_right(self, message):
+        self.right_state = message.data
+        self.last_right_message_time = self.get_clock().now()
         self.decide_navigation()
 
     def decide_navigation(self):
+        self.update_front_state()
         motor_command = String()
 
         if not self.camera_fresh or not self.lidar_fresh:
              motor_command.data = 'STOP'
 
-        elif self.camera_state == 'UNKNOWN' or self.lidar_state == 'UNKNOWN':
+        elif self.front_state == 'UNKNOWN':
              motor_command.data = 'STOP'
 
-        elif self.camera_state == 'OBSTACLE_DETECTED' or self.lidar_state == 'OBSTACLE_DETECTED':
-             motor_command.data = 'STOP'
+        elif self.front_state == 'OBSTACLE_DETECTED':
+             if self.left_fresh and self.left_state == 'PATH_CLEAR':
+                  motor_command.data = 'TURN_LEFT'
+             else:
+                  motor_command.data = 'STOP' 
+
       
-        elif self.camera_state == 'PATH_CLEAR' and self.lidar_state == 'PATH_CLEAR':
-             motor_command.data = 'MOVE_FORWARD' 
-        
+        elif self.front_state == 'PATH_CLEAR':
+             motor_command.data = 'MOVE_FORWARD'        
         else: 
              motor_command.data = 'STOP'
         
         self.motor_command_publisher.publish(motor_command)
         self.get_logger().info(
-             f'Camera: {self.camera_state}, LiDAR: {self.lidar_state}, Command: {motor_command.data}'
+             f'Camera: {self.camera_state}, LiDAR: {self.lidar_state}, Front: {self.front_state}, Command: {motor_command.data}'
         )
 
     def check_sensor_freshness(self):
@@ -84,7 +122,22 @@ class NavigationObstacleSubscriber(Node):
             self.lidar_fresh = False
             self.decide_navigation()
             return
-        
+
+        if self.last_left_message_time is None:
+            self.left_fresh = False 
+        else:
+            left_age = current_time - self.last_left_message_time
+            left_age_seconds = left_age.nanoseconds / 1_000_000_000
+            self.left_fresh = left_age_seconds <= self.sensor_timeout
+ 
+        if self.last_right_message_time is None:
+            self.right_fresh = False
+        else:
+            right_age = current_time - self.last_right_message_time
+            right_age_seconds = right_age.nanoseconds / 1_000_000_000
+            self.right_fresh = right_age_seconds <= self.sensor_timeout
+
+
         camera_age = current_time - self.last_camera_message_time
         lidar_age = current_time - self.last_lidar_message_time
 
@@ -95,6 +148,17 @@ class NavigationObstacleSubscriber(Node):
         self.lidar_fresh = lidar_age_seconds <= self.sensor_timeout
 
         self.decide_navigation()
+
+
+    def update_front_state(self):
+        if self.camera_state == 'OBSTACLE_DETECTED' or self.lidar_state == 'OBSTACLE_DETECTED':
+            self.front_state = 'OBSTACLE_DETECTED'
+
+        elif self.camera_state == 'PATH_CLEAR' and self.lidar_state == 'PATH_CLEAR': 
+            self.front_state = 'PATH_CLEAR'
+
+        else: 
+            self.front_state = 'UNKNOWN'
 
 def main(args=None):
     rclpy.init(args=args)
