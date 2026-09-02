0754426e2ccf5eb6e654c30325e3b8f90ac90696
Pradip <pradipmainai5@gmail.com>
2026-09-02 23:47:13 +0100

    cpp_learning

--- projects/ros2-restaurant-delivery-robot/cpp_learning/day40_cpp_basics.cpp
+++ projects/ros2-restaurant-delivery-robot/cpp_learning/day40_cpp_basics.cpp
@@ -10,6 +10,12 @@ struct RobotState
     bool emergency_status;
 };
 
+struct RobotConfig
+{
+    double safe_front_distance;
+    int low_battery_threshold;
+};
+
 enum class RobotCommand 
 {
     EMERGENCY_STOP,
@@ -21,44 +27,49 @@ enum class RobotCommand
 };
 
 
-bool is_battery_safe(int battery_percentage)
-{
-    if (battery_percentage > 20) {
-       return true;
-    }
-    else {
-        return false;
-    }
-}
+
+constexpr double SAFE_FRONT_DISTANCE = 1.5;
 
 bool is_front_path_clear(double obstacle_distance)
 {
-    if(obstacle_distance <= 1.5)
-        return  false;
-    else 
-        return true;
+    return obstacle_distance > SAFE_FRONT_DISTANCE;
+}
+
+constexpr int LOW_BATTERY_THRESHOLD = 20; 
+bool is_battery_safe(
+    int battery_percentage,
+    int low_battery_threshold
+)
+{ 
+    return battery_percentage > low_battery_threshold;
 }
 
 
-RobotCommand decide_movement(const RobotState& robot)
+RobotCommand decide_movement(
+    const RobotState& robot,
+    const RobotConfig& config 
+)
 {
     if (robot.emergency_status)
         return RobotCommand::EMERGENCY_STOP;
 
-    else if (!is_battery_safe(robot.battery_percentage))
+    if (!is_battery_safe(
+        robot.battery_percentage,
+        config.low_battery_threshold
+    ))
         return RobotCommand::LOW_BATTERY_STOP;
 
-    else if (is_front_path_clear(robot.obstacle_distance))
+    if (is_front_path_clear(robot.obstacle_distance))
         return RobotCommand::MOVE_FORWARD;
 
-    else if (robot.left_path)
+    if (robot.left_path)
         return RobotCommand::TURN_LEFT;
 
-    else if (robot.right_path)
+    if (robot.right_path)
         return RobotCommand::TURN_RIGHT;
+
+    return RobotCommand::PATH_BLOCKED_STOP;
  
-    else 
-        return RobotCommand::PATH_BLOCKED_STOP;
 }
 
 
@@ -102,8 +113,13 @@ int main() {
       robot.right_path = true ;
       robot.emergency_status = false;
 
+      RobotConfig config;
+ 
+      config.safe_front_distance = 1.5;
+      config.low_battery_threshold = 20;
 
-      RobotCommand movement_command = decide_movement(robot);
+
+      RobotCommand movement_command = decide_movement(robot, config);
       print_robot_command(movement_command);
       
       return 0;
